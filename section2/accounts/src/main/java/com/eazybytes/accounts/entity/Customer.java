bcfd4a14623939b9b8b9c9fcb52629cf30fe3caf
Tharuka Madhushanka <79903664+Tharu119@users.noreply.github.com>
2024-06-13 08:06:48 +0530

    Update

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.eazybytes.accounts.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+//import jakarta.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@ToString @AllArgsConstructor @NoArgsConstructor
+//@Table(name = "customer")
+public class Customer extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+    @Column(name="mobile_number")
+    private  String mobileNumber;
+
+
+}
